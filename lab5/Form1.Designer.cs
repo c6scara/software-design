4c862e9db9d4539cba68616fd9040101d77b7ac1
jbalbero.553703 <j.balbero.553703@umindanao.edu.ph>
2026-05-06 15:19:00 +0800

    update this shit idk

--- lab5/Form1.Designer.cs
+++ lab5/Form1.Designer.cs
@@ -20,20 +20,79 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnShowBooks = new Button();
+            listBoxBooks = new ListBox();
+            txtBookID = new TextBox();
+            btnUpdateBook = new Button();
+            SuspendLayout();
+            // 
+            // btnShowBooks
+            // 
+            btnShowBooks.Location = new Point(102, 213);
+            btnShowBooks.Name = "btnShowBooks";
+            btnShowBooks.Size = new Size(75, 23);
+            btnShowBooks.TabIndex = 0;
+            btnShowBooks.Text = "Show";
+            btnShowBooks.UseVisualStyleBackColor = true;
+            btnShowBooks.Click += btnShowBooks_Click;
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.ItemHeight = 15;
+            listBoxBooks.Location = new Point(295, 63);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(553, 409);
+            listBoxBooks.TabIndex = 1;
+            // 
+            // txtBookID
+            // 
+            txtBookID.Location = new Point(102, 125);
+            txtBookID.Name = "txtBookID";
+            txtBookID.Size = new Size(100, 23);
+            txtBookID.TabIndex = 2;
+            // 
+            // btnUpdateBook
+            // 
+            btnUpdateBook.Location = new Point(102, 291);
+            btnUpdateBook.Name = "btnUpdateBook";
+            btnUpdateBook.Size = new Size(75, 23);
+            btnUpdateBook.TabIndex = 3;
+            btnUpdateBook.Text = "Update";
+            btnUpdateBook.UseVisualStyleBackColor = true;
+            btnUpdateBook.Click += btnUpdateBook_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Every_End;
+            ClientSize = new Size(984, 561);
+            Controls.Add(btnUpdateBook);
+            Controls.Add(txtBookID);
+            Controls.Add(listBoxBooks);
+            Controls.Add(btnShowBooks);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnShowBooks;
+        private ListBox listBoxBooks;
+        private TextBox txtBookID;
+        private Button btnUpdateBook;
+    }
 }
--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -1,10 +1,79 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace lab5
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Author
+        {
+            public string Name { get; set; }
+
+            public int AuthorID { get; set; }
+            public virtual ICollection<Book> Books { get; set; }
+        }
+        public class Book
+        {
+            public string Title { get; set; }
+            public int BookID { get; set; }
+            public int AuthorID { get; set; }
+            public virtual Author Author { get; set; }
+        }
+
+        public void AddAuthorWithBook(string authorName, string bookTitle)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var author = new Author { Name = authorName };
+                var book = new Book { Title = bookTitle, Author = author };
+                context.Authors.Add(author);
+                context.Books.Add(book);
+                context.SaveChanges();
+            }
+        }
+        public List<string> GetBooksWithAuthors()
+        {
+            using (var context = new BookstoreContext())
+            {
+                var booksWithAuthors = context.Books
+                    .Include(b => b.Author)
+                    .Select(b => $"{b.Title} by {b.Author.Name}")
+                    .ToList();
+                return booksWithAuthors;
+            }
+        }
+        public void UpdateBookAndAuthor(int bookId, string newAuthorName, string newTitle)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var book = context.Books.Include(b => b.Author).FirstOrDefault(b => b.BookID == bookId);
+                if (book != null)
+                {
+                    book.Title = newTitle;
+                    book.Author.Name = newAuthorName;
+                    context.SaveChanges();
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShowBooks_Click(object sender, EventArgs e)
+        {
+            var books = GetBooksWithAuthors();
+            listBoxBooks.DataSource = books;
+        }
+
+        private void btnUpdateBook_Click(object sender, EventArgs e)
+        {
+            int bookId = int.Parse(txtBookID.Text);
+            UpdateBookAndAuthor(bookId, txtBookTitle.Text, txtAuthorName.Text);
+            MessageBox.Show("Book and Author updated successfully!");
+        }
+    }
 }
